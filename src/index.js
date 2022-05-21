4312ed0a879fb82d8b7cbf99b1c2ecf91166c505
Tasha Marion Wan <tas.wan23@gmail.com>
2022-05-21 10:17:01 +0800

    Change tab title

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './assets/fonts/fonts.css';
 import './index.css'
 import App from './App';
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <HashRouter>
+ <Routes>
+ <Route path="/" element={<App/>} />
+ </Routes>
+ </HashRouter>
   </React.StrictMode>
 );
 
