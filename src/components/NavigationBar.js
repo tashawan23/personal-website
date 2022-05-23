b4f661bbdb26661d0ec6ab12b0a54452dd02273f
Tasha Marion Wan <tas.wan23@gmail.com>
2022-05-23 20:50:27 +0800

    Fix styling

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,27 @@
 import './NavigationBar.css';
 import {Link} from 'react-scroll'
+import { useState, useEffect } from 'react';
 
 function NavigationBar() {
+    const [menu, setMenu] = useState(true)
+    const showMenu = () => {
+        if (window.innerWidth <= 767) {
+          setMenu(false);
+        } else {
+          setMenu(true);
+        }
+      };
+    
+      useEffect(() => {
+        showMenu();
+      }, []);
+    
+      window.addEventListener('resize', showMenu);
+
     return(
         <nav className="nav-bar">
             <div className="nav-name">Tasha Wan</div>
-            <ul className="nav-menu">
+            {menu && <ul className="nav-menu">
                 <li className="nav-links">
                     <Link to="experience" spy={true} smooth={true}>Experience</Link>
                 </li>
@@ -15,7 +31,7 @@ function NavigationBar() {
                 <li className="nav-links">
                 <Link to="skills" spy={true} smooth={true}>Skills</Link>
                 </li>
-            </ul>
+            </ul>}
         </nav>
     )
 }
